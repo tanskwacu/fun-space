c6f638e8da77375c7100dbbaf2d0c84b73588894
dindda <adindasalsabila3359@gmail.com>
2024-03-05 12:07:45 +0700

    RUBAH ADALAH

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ def luas_segitiga():
     a = int(input("masukkan alas segitiga: "))
     t = int(input("masukkan tinggi segitiga: "))
     luas = a * t / 2
-    print("luas segitiga adalah: ", luas)
+    print("luas segitiga ADALAH: ", luas)
 
 luas_segitiga()
